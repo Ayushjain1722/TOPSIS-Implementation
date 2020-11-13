3bec16187593b92c78279d5b6a5399a63642f6fb
Ayushjain1722 <42894689+Ayushjain1722@users.noreply.github.com>
2020-11-13 21:58:57 +0530

    Adding the project

---
+++ TOPSIS-Ayush_Jain-101803690/exceptions.py
@@ -0,0 +1,4 @@
+class ObjectNotFound(Exception):
+    """ An Exception risen when an expected object is not found """
+class InvalidOperation(Exception):
+    """ An Exception risen when an attempted operation does not make sense """
